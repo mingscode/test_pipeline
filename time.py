023ef73c9afb26b79fd31ce214d36d9e300459ec
mingscode <techway.lee@gmail.com>
2018-07-03 13:53:48 -0400

    auto deploy test v12

--- time.py
+++ time.py
@@ -17,7 +17,7 @@ def mainpage():
               <input type="submit" value="submit">
               </form>
 	      <p>{1}</p>
-              <p>This is version 1.8</p>
+              <p>This is version 1.9</p>
               '''.format(name, hello)
     footer = '</body></html>'
     return header + body + footer
